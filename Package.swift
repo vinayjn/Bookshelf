a244e68f040c872fb9d64b3e71586678b4cdcf5e
Vinay Jain <vinay.jn7@gmail.com>
2021-09-08 18:39:01 +0530

    Added Stencil for template strings

--- Package.swift
+++ Package.swift
@@ -16,6 +16,7 @@ let package = Package(
     // Dependencies declare other packages that this package depends on.
     // .package(url: /* package url */, from: "1.0.0"),    
     .package(url: "https://github.com/tid-kijyun/Kanna.git", from: "5.2.4"),
+    .package(url: "https://github.com/stencilproject/Stencil.git", from: "0.14.0")
   ],
   targets: [
     // Targets are the basic building blocks of a package. A target can define a module or a test suite.
@@ -23,7 +24,8 @@ let package = Package(
     .target(
       name: "Bookshelf",
       dependencies: [
-        "Kanna"        
+        "Kanna",
+        "Stencil"
       ]),
     .testTarget(
       name: "BookshelfTests",
