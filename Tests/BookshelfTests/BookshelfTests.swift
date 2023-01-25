893470054bfa05e23d335bf57482b0240cc79a76
Vinay <vinay.jn7@gmail.com>
2023-01-25 16:27:29 +0100

    Updated tests for Scrappers

--- Tests/BookshelfTests/BookshelfTests.swift
+++ Tests/BookshelfTests/BookshelfTests.swift
@@ -2,65 +2,75 @@
 import Stencil
 import XCTest
 
-final class FileHandlerMock: FileHandlerProtocol {
-    func getPageTemplate() throws -> String {
-        "{{content}}"
+final class BookshelfTests: XCTestCase {
+  
+  func testBooksJSONParsing() throws {
+    let data = try XCTUnwrap(SampleJSON.books.data(using: .utf8))
+    let sections = try JSONDecoder().decode([NewShelfSection].self, from: data)
+    XCTAssertNotNil(sections)
+    
+    XCTAssertEqual(sections.count, 2)
+    let currentlyReading = try XCTUnwrap(sections.first)    
+    let currentlyPending = currentlyReading.books.filter { state in
+      if case .pending = state {
+        return true
+      }
+      return false
     }
+    
+    XCTAssertEqual(currentlyPending.count, 1)
+    XCTAssertEqual(currentlyReading.books.count, 4)
+    XCTAssertEqual(currentlyReading.header, "Currently Reading")
+  }
+  
+  func testHTMLGeneration() {
+    let generated = try? HTMLGenerator.generate(
+      sections: [
+        ShelfSection(
+          header: "Reading",
+          books: [
+            Book(
+              goodreadsURL: "www.goodreads.com",
+              title: "Steve Jobs",
+              imageURL: "",
+              authors: ["Walter Issacson"]
+            ),
+          ]
+        ),
+      ])
 
-    func getSectionTemplate() throws -> String {
-        "Header: {{header}}\n Books: {{books}}"
-    }
-
-    func getBookTemplate() throws -> String {
-        "Title: {{title}} Subtitle: {{subtitle}} Goodreads: {{goodreadsURL}} Authors: {{authors}} Cover: {{cover}}"
-    }
+    XCTAssertNotNil(generated)
+  }
+  
+  func testISBNScrapper() async throws {
+    var scrapper: WebScrapper = ISBNSearchScrapper(isbn: "9781099617201")
+    let book = try await scrapBook(with: &scrapper)
+    XCTAssertNotNil(book)
+  }
+  
+  func testAmazonScrapper() async throws {
+    var scrapper: WebScrapper = AmazonScrapper(pid: "1608685489")
+    let book = try await scrapBook(with: &scrapper)
+    XCTAssertNotNil(book)
+  }
+  
+  static var allTests = [
+    ("testBooksJSONParsing", testHTMLGeneration),
+    ("testHTMLGeneration", testHTMLGeneration),
+    ("testISBNScrapper", testISBNScrapper),
+    ("testAmazonScrapper", testAmazonScrapper),
+  ]
 }
 
-final class BookshelfTests: XCTestCase {
-    func testHTMLGeneration() {
-        let generator = HTMLGenerator(fileHandler: FileHandlerMock())
-        let generated = try? generator.generate(
-            sections: [
-                ShelfSection(
-                    header: "Reading",
-                    books: [
-                        Book(
-                            goodreadsURL: "www.goodreads.com",
-                            title: "Steve Jobs",
-                            imageURL: "",
-                            authors: ["Walter Issacson"]
-                        ),
-                    ]
-                ),
-            ])
-
-        XCTAssertNotNil(generated)
-    }
-
-    func testISBNScrapper() async throws {
-        let url = URL(string: "https://isbnsearch.org/isbn/9781099617201")!
-        var scrapper: WebScrapper = ISBNSearchScrapper(url: url)
-        let book = try await scrapBook(with: &scrapper)
-
-        XCTAssertNotNil(book)
-    }
-
-    func testAmazonScrapper() async throws {
-        let url = URL(string: "https://www.amazon.co.uk/Clean-Architecture-Craftsmans-Software-Structure/dp/0134494164")!
-        var scrapper: WebScrapper = AmazonScrapper(url: url)
-        let book = try await scrapBook(with: &scrapper)
-        print(book)
-        XCTAssertNotNil(book)
-    }
-
-    func scrapBook(with scrapper: inout WebScrapper) async throws -> Book {
-        try await scrapper.scrap()
-        return try scrapper.buildBook()
-    }
-
-    static var allTests = [
-        ("testHTMLGeneration", testHTMLGeneration),
-        ("testISBNScrapper", testISBNScrapper),
-        ("testAmazonScrapper", testAmazonScrapper),
-    ]
+private extension BookshelfTests {
+  
+  private func jsonData(from string: String) throws -> Data {
+    return try XCTUnwrap(string.data(using: .utf8))
+  }
+  
+  func scrapBook(with scrapper: inout WebScrapper) async throws -> NewBook {
+    try await scrapper.scrap()
+    return try scrapper.buildBook()
+  }
+  
 }
---
+++ Tests/BookshelfTests/SampleJSON.swift
@@ -0,0 +1,53 @@
+import Foundation
+
+enum SampleJSON {
+  
+  static let books = """
+  [
+  {
+    "books": [
+      {
+        "pid": "1608685489",
+        "isbn": "9781608685486"
+      },
+      {
+        "title": "Fluent Forever: How to Learn Any Language Fast and Never Forget It",
+        "imageURL": "images/bookshelf/DBED2811-8F95-43B2-B1CF-7110A2ADD75E.jpg",
+        "affiliateURL": "https://www.goodreads.com/book/show/19661852-fluent-forever",
+        "authors": [
+          "Gabriel Wyner"
+        ]
+      },
+      {
+        "title": "Recursion",
+        "imageURL": "images/bookshelf/5DC2CB0A-AA8D-4A27-90ED-238434E972DE.jpg",
+        "affiliateURL": "https://www.goodreads.com/book/show/43592998-recursion",
+        "authors": [
+          "Blake Crouch"
+        ]
+      },
+      {
+        "title": "Refactoring UI",
+        "imageURL": "images/bookshelf/17034B44-2F7C-4658-8B29-41C3A5AECB99.jpg",
+        "affiliateURL": "https://www.goodreads.com/book/show/43190966-refactoring-ui",
+        "authors": [
+          "Adam Wathan",
+          "Steve Schoger"
+        ]
+      }
+    ],
+    "header": "Currently Reading"
+  },
+  {
+    "books": [
+      {
+        "pid": "1099617200",
+        "isbn": "9781099617201"
+      }
+    ],
+    "header": "2023"
+  }
+  ]
+  """
+  
+}
